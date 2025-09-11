d62f72147ff70f147390edeafb806a67773f267c
Rosana Damasceno <44578894+Rosanaxica@users.noreply.github.com>
2025-09-10 21:58:04 -0300

    feat(musica)adicionando musica na app

--- app/index.jsx
+++ app/index.jsx
@@ -1,52 +1,106 @@
-import { useState } from "react";
-import { Text, View, StyleSheet, Image, Pressable } from "react-native";
+import { useRef, useState } from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
 import { FokusButton } from "../components/FokusButton";
 import { ActionButton } from "../components/ActionButton";
-const pomodoro = [{
-  id: "foco",
-  initialValue: 25,
-  image: require("./foco.png"),
-  name: "Foco"
-},
-{
-  id: "curto",
-  initialValue: 5,
-  image: require("./curto.png"),
-  name: "Pausa Curta"
-},
-{
-  id: "longo",
-  initialValue: 15,
-  image: require("./longo.png"),
-  name: "Pausa Longa"
-}]
-export default function Index() {
-  const [timerType, setTimerType] = useState(pomodoro[0])
-  return (
-    <View
-      style={styles.container}
-    >
-      <Image
-        source={timerType.image}
+import { Timer } from "../components/Timer";
+import { IconPause, IconPlay } from "../components/Icons";
 
-      />
-      <View style={styles.actions} >
+import { useAudioPlayer } from "expo-audio";
+
+const pomodoro = [
+  {
+    id: "foco",
+    initialValue: 25 * 60,
+    image: require("./foco.png"),
+    name: "Foco",
+  },
+  {
+    id: "curto",
+    initialValue: 5 * 60,
+    image: require("./curto.png"),
+    name: "Pausa Curta",
+  },
+  {
+    id: "longo",
+    initialValue: 15 * 60,
+    image: require("./longo.png"),
+    name: "Pausa Longa",
+  },
+];
+export default function Index() {
+  const audioSource = require("./tick.mp3");
+  const audioSourceEnd = require("./alarm.mp3");
+  const player = useAudioPlayer(audioSource);
+  const playerEnd = useAudioPlayer(audioSourceEnd);
+
+  const [timerType, setTimerType] = useState(pomodoro[0]);
+  const [timerRunning, setTimerRunning] = useState(false);
+  const [seconds, setSeconds] = useState(pomodoro[0].initialValue);
+
+  const clear = () => {
+    if (timerRef.current != null) {
+      clearInterval(timerRef.current);
+      timerRef.current = null;
+      setTimerRunning(false);
+      player.pause();
+    }
+  };
+
+  const timerRef = useRef(null);
+
+  const toggleTimerType = (newTimerType) => {
+    setTimerType(newTimerType);
+    setSeconds(newTimerType.initialValue);
+    clear();
+  };
+
+  const toggleTimer = () => {
+    if (timerRef.current) {
+      clear();
+      return;
+    }
+    setTimerRunning(true);
+    const id = setInterval(() => {
+      setSeconds((oldState) => {
+        if (oldState === 0) {
+          clear();
+          player.pause();
+          playerEnd.play();
+          return timerType.initialValue;
+        }
+        player.play();
+
+        return oldState - 1;
+      });
+    }, 1000);
+    timerRef.current = id;
+  };
+  return (
+    <View style={styles.container}>
+      <Image source={timerType.image} />
+      <View style={styles.actions}>
         <View style={styles.context}>
-          {pomodoro.map(p => (
-            <ActionButton key={p.id} active={timerType.id === p.id} onPress={()=>setTimerType(p)} name={p.name}></ActionButton>
+          {pomodoro.map((p) => (
+            <ActionButton
+              key={p.id}
+              active={timerType.id === p.id}
+              onPress={() => toggleTimerType(p)}
+              name={p.name}
+            ></ActionButton>
           ))}
         </View>
-        <Text style={styles.timer}>{new Date(timerType.initialValue * 1000).toLocaleTimeString("pt-BR", { minute: "2-digit", second: "2-digit" })}</Text>
-
-        <FokusButton />
+        <Timer totalSeconds={seconds} />
+        <FokusButton
+          title={timerRunning ? "Pausar" : "Começar"}
+          icon={timerRunning ? <IconPause /> : <IconPlay />}
+          onPress={toggleTimer}
+        />
       </View>
       <View style={styles.footer}>
         <Text style={styles.footerText}>
           Projeto fictício e sem fins comerciais.
         </Text>
-        <Text style={styles.footerText} >
-          Desenvolvido por Alura.
-        </Text>
+        <Text style={styles.footerText}>Desenvolvido por Alura.</Text>
       </View>
     </View>
   );
@@ -67,27 +121,19 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     width: "80%",
     borderColor: "#144480",
-    gap: 32
+    gap: 32,
   },
   context: {
     flexDirection: "row",
     justifyContent: "space-around",
-    alignItems: "center"
-  },
-
-  timer: {
-    fontWeight: "bold",
-    fontSize: 54,
-    color: "#FFF",
-    textAlign: "center"
+    alignItems: "center",
   },
   footer: {
-    width: "80%"
+    width: "80%",
   },
   footerText: {
     textAlign: "center",
     color: "#98A0a8",
-    fontSize: 12.5
-  }
+    fontSize: 12.5,
+  },
 });
-
